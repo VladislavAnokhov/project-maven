c9b00d40051879ca41c665d294cad956ad7a8515
Anokov Vladislav <anokhovvladisav@gmail.com>
2023-08-24 18:51:00 +0300

    the mission has done

--- src/test/java/StrangeTest.java
+++ src/test/java/StrangeTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.fail;
 public class StrangeTest {
 
     @Test
-    public void shouldFail() {
-        fail("By design");
+   public void shouldFail() {
+       fail("By design");
     }
 }
